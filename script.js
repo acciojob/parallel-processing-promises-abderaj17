28908780ef8567a2411baac5453625f0e5154c4c
Adarsh Agrahari <115093974+abderaj17@users.noreply.github.com>
2024-11-13 16:25:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,3 +7,22 @@ const images = [
   { url: "https://picsum.photos/id/238/200/300" },
   { url: "https://picsum.photos/id/239/200/300" },
 ];
+
+btn.addEventListener('click', (()=>{
+	const imagePromises = images.map(images =>{
+		return new Promise((resolve, reject)=>{
+			let imgElement = new Image();
+			imgElement.src = images.url;
+			imgElement.onload = ()=> resolve(imgElement);
+			imgElement.onerror = ()=> reject(`Failed to load image's URL: ${images.url}`);
+		});
+	});
+	Promise.all(imagePromises)
+	.then(imgElement =>{
+		imgElement.forEach(imgElement=>{
+			output.appendChild(imgElement);
+		});
+	}).catch(error=>{
+		console.error(error);
+	})
+}))
